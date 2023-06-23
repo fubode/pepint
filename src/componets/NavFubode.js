31251ca72759a4645b511450ef6ed6abe7cc4fd5
Juan pablo <201400076@est.umss.edu>
2023-06-23 05:02:26 -0400

    ajustes de logo

--- src/componets/NavFubode.js
+++ src/componets/NavFubode.js
@@ -1,11 +1,11 @@
 import React from "react";
-import logo from "../assets/img/logo.png";
+import logo from "../assets/img/fubode-768x450.webp";
 export const NavFubode = () => {
   return (
     <div>      
       <div className="container-fluid">      
         <div className="row">
-          <div className="col-2"><img src={logo} alt="Fubode logo" /></div>
+          <div className="col-2"><img src={logo} alt="Fubode logo" style={{ width: '150px' }}/></div>
           <div className="col-10">
             <div className="text-center">
             <h3>

ebb608abb03ad3b189a6ea4bd0be0e4599723b9e
Juan pablo <201400076@est.umss.edu>
2023-09-11 23:57:05 -0400

    cambio de contraseña

--- src/componets/Password.js
+++ src/componets/Password.js
@@ -1,47 +1,120 @@
-import React, { PureComponent } from 'react';
-//import bcrypt from 'bcrypt';
+import React, { PureComponent, useState } from 'react';
+import { Button, Modal } from 'react-bootstrap';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import { useSolicitud } from '../context/SolicitudContext';
 
-export class Password extends PureComponent {
-  state = {
-    oldPassword: '',
-    newPassword: '',
+const Password = () => {
+  const [oldPassword, setOldPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
   };
+  const { actualizarPasswordSupa,showMessageSucces } = useSolicitud();
+  const [error, setError] = useState('');
 
-  handleOldPasswordChange = (event) => {
-    this.setState({ oldPassword: event.target.value });
-  };
-
-  handleNewPasswordChange = (event) => {
-    this.setState({ newPassword: event.target.value });
-  };
-
-  actualizarPassword = async () => {
-    const { oldPassword, newPassword } = this.state;
-
-    // Encriptar el nuevo password
-    const saltRounds = 10;
-    //const hashedPassword = await bcrypt.hash(newPassword, saltRounds);
-
+  const actualizarPassword = () => {
+    setError('');
+    console.log(oldPassword, newPassword);
     if (oldPassword === newPassword) {
-      console.log('Los passwords sson iguales, se ejecutará la actualización.');
-      //console.log('Password encriptado:', hashedPassword);
-      // Lógica para actualizar el password en tu aplicación usando el hashedPassword
-    } else {
-      console.log('Los passwords no son iguales, no se realizará la actualización.');
+      setError('La nueva contraseña no puede ser igual a la anterior.');
+      return;
     }
-  };
 
-  render() {
-    return (
-      <>
-        <p>Password antiguo</p>
-        <input type='text' value={this.state.oldPassword} onChange={this.handleOldPasswordChange} />
-        <p>Password nuevo</p>
-        <input type='text' value={this.state.newPassword} onChange={this.handleNewPasswordChange} />
-        <button onClick={this.actualizarPassword}>Actualizar Password</button>
-      </>
-    );
+    // Verificar que newPassword tenga al menos 6 caracteres
+    if (newPassword.length < 6) {
+      setError('La nueva contraseña debe tener al menos 6 caracteres.');
+      return;
+    }
+
+    // Verificar que newPassword contenga al menos una letra mayúscula
+    if (!/[A-Z]/.test(newPassword)) {
+      setError('La nueva contraseña debe contener al menos una letra mayúscula.');
+      return;
+    }
+
+    // Verificar que newPassword contenga al menos una letra minúscula
+    if (!/[a-z]/.test(newPassword)) {
+      setError('La nueva contraseña debe contener al menos una letra minúscula.');
+      return;
+    }
+
+    // Verificar que newPassword contenga al menos un número
+    if (!/\d/.test(newPassword)) {
+      setError('La nueva contraseña debe contener al menos un número.');
+      return;
+    }
+
+    // Verificar que newPassword contenga al menos un carácter especial
+    if (!/[@#$%^&+=!]/.test(newPassword)) {
+      setError('La nueva contraseña debe contener al menos un carácter especial (por ejemplo, @, #, $, %, ^, &, +, =, !).');
+      return;
+    }
+    
+    // Si todas las verificaciones pasan, la contraseña es válida
+    console.log('Contraseña actualizada correctamente.');
+    actualizarPasswordSupa(oldPassword,newPassword);
   }
+
+  return (
+    <>
+      <div className="card-body mt-4 text-center">
+        <div className="form-outline mb-4">
+          <input
+            type={showPassword ? 'text' : 'password'}
+            name="password"
+            id="typeEmailX-2"
+            className="form-control form-control-lg"
+            onChange={(e) => setOldPassword(e.target.value)}
+            value={oldPassword}
+          />
+          <label className="form-label" htmlFor="typeEmailX-2">
+            Contraseña
+          </label>
+          <span className="password-toggle-icon m-4" onClick={toggleShowPassword}>
+            {showPassword ? <FaEyeSlash /> : <FaEye />}
+          </span>
+        </div>
+        <div className="form-outline mb-4">
+          <input
+            type={showPassword ? 'text' : 'password'}
+            id="typePasswordX-2"
+            name="password"
+            className="form-control form-control-lg"
+            onChange={(e) => setNewPassword(e.target.value)}
+            value={newPassword}
+          />
+          <label className="form-label" htmlFor="typePasswordX-2">
+            Repita la Contraseña
+          </label>
+          <span className="password-toggle-icon m-4" onClick={toggleShowPassword}>
+            {showPassword ? <FaEyeSlash /> : <FaEye />}
+          </span>
+          {error && <div className="error-message" style={{ color: 'red' }}>{error}</div>}
+        </div>
+      </div>
+      <Button className="" onClick={actualizarPassword}>
+        Actualizar contraseña
+      </Button>
+      <Modal show={showMessageSucces} onHide={()=>{}}>
+        <Modal.Header
+          className="bg-success"
+          closeButton
+        >
+          <Modal.Title>{"FELICIDADES"}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>{"La contraseña fue cambiada con exito"}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="danger" onClick={()=>{}}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+    </>
+  )
 }
 
-export default Password;
+export default Password
--- src/pages/Uif/Uif.js
+++ src/pages/Uif/Uif.js
@@ -1,12 +1,12 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import NavFubode from "../../componets/NavFubode";
 import { useSolicitud } from '../../context/SolicitudContext';
 import { CardConsultor } from '../../componets/Consultor/CardConsultor';
 import Paginacion from '../../componets/Paginacion';
 import Recargar from '../../componets/Consultor/Recargar';
+import { useNavigate } from 'react-router-dom';
 
 const Uif = () => {
-
   const {
     solicitudes,
     getFuncionario,
@@ -15,10 +15,47 @@ const Uif = () => {
     getCorreos
   } = useSolicitud();
 
+  const navigate = useNavigate();
+
+  const checkForInactivity = () =>{
+    const expireTime = localStorage.getItem("expireTime");
+    const currentDate = Date.now();
+    if(expireTime < currentDate){
+      navigate("/");
+    }
+  }
+
+  const updateExpireTime = () =>{
+    const expireTime = Date.now()+360000;
+    localStorage.setItem("expireTime",expireTime)
+  }
+
+
+  useEffect(() => {
+    const interval = setInterval(()=>{
+      checkForInactivity();
+    },10000);
+    
+    return () => clearInterval(interval);
+  }, [])
+  
   useEffect(() => {
     getFuncionario();
     getCorreos();
     getSolicitudesUIF();
+
+    updateExpireTime();
+    window.addEventListener("click",updateExpireTime);
+    window.addEventListener("keypress",updateExpireTime);
+    window.addEventListener("scroll",updateExpireTime);
+    window.addEventListener("mousemove",updateExpireTime);
+
+    return ()=>{
+      window.removeEventListener("click",updateExpireTime);
+      window.removeEventListener("keypress",updateExpireTime);
+      window.removeEventListener("scroll",updateExpireTime);
+      window.removeEventListener("mousemove",updateExpireTime);  
+    }
   }, []);
   return (
     <>
